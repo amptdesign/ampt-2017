f84d2b60d79d23427373c287e47a7490c33ff792
Ampt Design Admin <amptdesign@live.com>
2016-07-28 08:35:54 -0500

    SLIDES LOOK AWESOME!!! YAY!!!

---
+++ HEADER-STYLE-ORIG.php
@@ -0,0 +1,897 @@
+<style type="text/css">
+::-moz-selection {
+    background: #77be32;
+}
+
+::selection {
+    background: #77be32;
+}
+
+a,
+.ha-transparent #navigation ul li a:hover {
+    color: #77be32;
+}
+
+.ut-language-selector a:hover {
+    color: #77be32;
+}
+
+.ut-custom-icon-link:hover i {
+    color: #77be32 !important;
+}
+
+.ut-hide-member-details:hover,
+#ut-blog-navigation a:hover,
+.light .ut-hide-member-details,
+.ut-mm-button:hover:before,
+.ut-mm-trigger.active .ut-mm-button:before,
+.ut-mobile-menu a:after {
+    color: #77be32;
+}
+
+.ut-header-light .ut-mm-button:before {
+    color: #77be32;
+}
+
+.lead span,
+.entry-title span,
+#cancel-comment-reply-link,
+.member-description-style-3 .ut-member-title,
+.ut-twitter-rotator h2 a,
+.themecolor {
+    color: #77be32;
+}
+
+.icons-ul i,
+.comments-title span,
+.member-social a:hover,
+.ut-parallax-quote-title span,
+.ut-member-style-2 .member-description .ut-member-title {
+    color: #77be32;
+}
+
+.about-icon,
+.ut-skill-overlay,
+.ut-dropcap-one,
+.ut-dropcap-two,
+.ut-mobile-menu a:hover,
+.themecolor-bg,
+.ut-btn.ut-pt-btn:hover,
+.ut-btn.dark:hover {
+    background: #77be32;
+}
+
+blockquote,
+div.wpcf7-validation-errors,
+.ut-hero-style-5 .hero-description,
+#navigation ul.sub-menu,
+.ut-member-style-3 .member-social a:hover {
+    border-color: #77be32;
+}
+
+.cta-section,
+.ut-btn.theme-btn,
+.ut-social-link:hover .ut-social-icon,
+.ut-member-style-2 .ut-so-link:hover {
+    background: #77be32 !important;
+}
+
+.ut-social-title,
+.ut-service-column h3 span,
+.ut-rq-icon {
+    color: #77be32;
+}
+
+.count,
+.ut-icon-list i {
+    color: #77be32;
+}
+
+.client-section,
+.ut-portfolio-pagination.style_two a.selected:hover,
+.ut-portfolio-pagination.style_two a.selected,
+.ut-portfolio-pagination.style_two a:hover,
+.ut-pt-featured {
+    background: #77be32 !important;
+}
+
+ins,
+mark,
+.ut-alert.themecolor,
+.ut-portfolio-menu.style_two li a:hover,
+.ut-portfolio-menu.style_two li a.selected,
+.light .ut-portfolio-menu.style_two li a.selected:hover {
+    background: #77be32;
+}
+
+.footer-content i {
+    color: #77be32;
+}
+
+.copyright a:hover,
+.footer-content a:hover,
+.toTop:hover,
+.ut-footer-dark a.toTop:hover {
+    color: #77be32;
+}
+
+blockquote span {
+    color: #77be32;
+}
+
+.entry-meta a:hover,
+#secondary a:hover,
+.page-template-templatestemplate-archive-php a:hover {
+    color: #77be32;
+}
+
+h1 a:hover,
+h2 a:hover,
+h3 a:hover,
+h4 a:hover,
+h5 a:hover,
+h6 a:hover,
+.ut-header-dark .site-logo .logo a:hover {
+    color: #77be32;
+}
+
+a.more-link:hover,
+.fa-ul li .fa {
+    color: #77be32;
+}
+
+.ut-pt-featured-table .ut-pt-info .fa-li {
+    color: #77be32 !important;
+}
+
+.button,
+input[type="submit"],
+input[type="button"],
+.dark button,
+.dark input[type="submit"],
+.dark input[type="button"],
+.light .button,
+.light input[type="submit"],
+.light input[type="button"] {
+    background: #77be32;
+}
+
+.img-hover {
+    background: rgb(119, 190, 50);
+    background: rgba(119, 190, 50, 0.85);
+}
+
+.portfolio-caption {
+    background: rgb(119, 190, 50);
+    background: rgba(119, 190, 50, 0.85);
+}
+
+.team-member-details {
+    background: rgba(119, 190, 50, 0.85);
+}
+
+.ut-avatar-overlay {
+    background: rgb(119, 190, 50);
+    background: rgba(119, 190, 50, 0.85);
+}
+
+.mejs-controls .mejs-time-rail .mejs-time-current,
+.mejs-controls .mejs-horizontal-volume-slider .mejs-horizontal-volume-current,
+.format-link .entry-header a {
+    background: #77be32 !important;
+}
+
+.light .ut-portfolio-menu li a:hover,
+.light .ut-portfolio-pagination a:hover,
+.light .ut-nav-tabs li a:hover,
+.light .ut-accordion-heading a:hover {
+    border-color: #77be32 !important;
+}
+
+.light .ut-portfolio-menu li a:hover,
+.light .ut-portfolio-pagination a:hover,
+.ut-portfolio-list li strong,
+.light .ut-nav-tabs li a:hover,
+.light .ut-accordion-heading a:hover,
+.ut-custom-icon a:hover i:first-child {
+    color: #77be32 !important;
+}
+
+.ut-portfolio-gallery-slider .flex-direction-nav a,
+.ut-gallery-slider .flex-direction-nav a,
+.ut-rotate-quote-alt .flex-direction-nav a,
+.ut-rotate-quote .flex-direction-nav a {
+    background: rgb(119, 190, 50);
+    background: rgba(119, 190, 50, 0.85);
+}
+
+.light .ut-bs-wrap .entry-title a:hover,
+.light .ut-bs-wrap a:hover .entry-title {
+    color: #77be32;
+}
+
+.ut-rated i {
+    color: #77be32;
+}
+
+.ut-footer-area ul.sidebar a:hover {
+    color: #77be32;
+}
+
+.ut-footer-dark .ut-footer-area .widget_tag_cloud a:hover {
+    color: #77be32!important;
+}
+
+.ut-footer-dark .ut-footer-area .widget_tag_cloud a:hover {
+    border-color: #77be32;
+}
+
+.elastislide-wrapper nav span:hover {
+    border-color: #77be32;
+}
+
+.elastislide-wrapper nav span:hover {
+    color: #77be32;
+}
+
+.ut-footer-so li a:hover {
+    border-color: #77be32;
+}
+
+.ut-footer-so li a:hover i {
+    color: #77be32!important;
+}
+
+.ut-pt-wrap.ut-pt-wrap-style-2 .ut-pt-featured-table .ut-pt-header {
+    background: #77be32;
+}
+
+.ut-pt-wrap-style-3 .ut-pt-info ul li,
+.ut-pt-wrap-style-3 .ut-pt-info ul,
+.ut-pt-wrap-style-3 .ut-pt-header,
+.ut-pt-wrap-style-3 .ut-btn.ut-pt-btn,
+.ut-pt-wrap-style-3 .ut-custom-row,
+.ut-pt-wrap-style-3 .ut-pt-featured-table .ut-btn {
+    border-color: rgba(119, 190, 50, 0.1);
+}
+
+.ut-pt-wrap-style-3 .ut-btn {
+    color: #77be32 !important;
+}
+
+.ut-pt-wrap-style-3 .ut-btn {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.ut-pt-wrap-style-3 .ut-pt-featured-table .ut-btn {
+    color: #77be32 !important;
+}
+
+.ut-pt-wrap-style-3 .ut-pt-featured-table .ut-btn {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.ut-pt-wrap-style-3 .ut-pt-featured-table .ut-pt-title {
+    color: #77be32 !important;
+}
+
+.ut-pt-wrap-style-3 .ut-pt-featured-table .ut-pt-title {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.ut-glow {
+    color: #77be32;
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption {
+    border-color: rgba(119, 190, 50, 1);
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption i {
+    border-color: rgba(119, 190, 50, 0.3);
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption i {
+    color: rgba(119, 190, 50, 0.3);
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption:hover i {
+    border-color: rgba(119, 190, 50, 1);
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption:hover i {
+    color: #77be32 !important;
+}
+
+.light .ut-shortcode-video-wrap .ut-video-caption:hover i {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.light .ut-video-loading {
+    color: #77be32 !important;
+}
+
+.light .ut-video-loading {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+.light .ut-video-caption-text {
+    border-color: rgba(119, 190, 50, 1);
+}
+
+.js #main-content section .section-content,
+.js #main-content section .section-header-holder {
+    opacity: 0;
+}
+
+#navigation {
+    font-family: "ralewaysemibold", Helvetica, Arial, sans-serif !important;
+}
+
+#navigation li a:hover {
+    color: #77be32;
+}
+
+#navigation .selected,
+#navigation ul li.current_page_parent a.active,
+#navigation ul li.current-menu-ancestor a.active {
+    color: #77be32;
+}
+
+#navigation ul li a:hover,
+#navigation ul.sub-menu li a:hover {
+    color: #77be32;
+}
+
+#navigation ul.sub-menu li > a {
+    color: #999999;
+}
+
+#navigation ul li.current-menu-item:not(.current_page_parent) a,
+#navigation ul li.current_page_item:not(.current_page_parent) a {
+    color: #77be32;
+}
+
+#navigation ul li.current-menu-item:not(.current_page_parent) .sub-menu li a {
+    color: #999999;
+}
+
+.ut-header-dark #navigation li a:hover {
+    color: #77be32;
+}
+
+.ut-header-dark #navigation .selected,
+.ut-header-dark #navigation ul li.current_page_parent a.active,
+.ut-header-dark #navigation ul li.current-menu-ancestor a.active {
+    color: #77be32;
+}
+
+.ut-header-dark #navigation ul li a:hover,
+.ut-header-dark #navigation ul.sub-menu li a:hover {
+    color: #77be32;
+}
+
+.ut-header-dark #navigation ul.sub-menu li > a {
+    color: #999999;
+}
+
+.ut-header-dark #navigation ul li.current-menu-item:not(.current_page_parent) a,
+.ut-header-dark #navigation ul li.current_page_item:not(.current_page_parent) a {
+    color: #77be32;
+}
+
+.ut-header-dark #navigation ul li.current-menu-item:not(.current_page_parent) .sub-menu li a {
+    color: #999999;
+}
+
+.ut-loader-overlay {
+    background: #ffffff
+}
+
+.hero-title span:not(.ut-word-rotator) {
+    color: #77be32 !important;
+}
+
+.hero-title.ut-glow span {
+    text-shadow: 0 0 40px #77be32, 2px 2px 3px black;
+}
+
+body {
+    font-family: ralewayregular, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h1 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h2 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h3 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h4 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h5 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+h6 {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.lg-progress-bar .lg-progress {
+    background-color: #77be32;
+}
+
+.lg-outer .lg-thumb-item.active,
+.lg-outer .lg-thumb-item:hover {
+    border-color: #77be32;
+}
+
+.hero-title {
+    text-transform: uppercase;
+}
+
+.hero-title {
+    font-family: ralewaysemibold, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.hero .parallax-overlay {
+    background-color: rgba(44, 62, 80, 0.1);
+}
+
+#contact-section .parallax-title {
+    color: #77BE32
+}
+
+#contact-section .pt-style-6 .page-title:after,
+#contact-section .pt-style-6 .parallax-title:after,
+#contact-section .pt-style-6 .section-title:after {
+    border-bottom: 1px dotted #77BE32;
+}
+
+#contact-section .parallax-title span span {
+    color: #77be32;
+}
+
+#contact-section .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#contact-section .ut-left-footer-area {
+    background: rgb(255, 255, 255, 1);
+}
+
+#contact-section .ut-left-footer-area {
+    background: rgba(255, 255, 255, 1);
+}
+
+#contact-section .parallax-overlay {
+    background: rgb(255, 255, 255, 1);
+}
+
+#contact-section .parallax-overlay {
+    background: rgba(255, 255, 255, 1);
+}
+
+#contact-section .parallax-overlay {
+    padding-top: 60px;
+    padding-bottom: 120px;
+}
+
+#contact-section .ut-offset-anchor {
+    top: -79px;
+}
+
+blockquote {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.lead,
+.taxonomy-description {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#ut-loader-logo {
+    background-image: url(http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/uploads/2016/06/bklyn-logo-dark.png);
+    background-position: center center;
+    background-repeat: no-repeat;
+}
+
+#about-brooklyn {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#about-brooklyn .ut-offset-anchor {
+    top: -159px;
+}
+
+#about-brooklyn .parallax-header {
+    padding-bottom: 30px;
+}
+
+#about-brooklyn .section-header {
+    padding-bottom: 30px;
+}
+
+#about-brooklyn .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#about-brooklyn .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#about-brooklyn {
+    background-color: #FFFFFF;
+}
+
+#about-brooklyn .section-header.pt-style-1 .section-title span {
+    background-color: #FFFFFF;
+}
+
+#some-of-the-clients-we-work-with .parallax-overlay {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#some-of-the-clients-we-work-with .ut-offset-anchor {
+    top: -79px;
+}
+
+#some-of-the-clients-we-work-with .parallax-header {
+    padding-bottom: 30px;
+}
+
+#some-of-the-clients-we-work-with .section-header {
+    padding-bottom: 30px;
+}
+
+#some-of-the-clients-we-work-with .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#some-of-the-clients-we-work-with .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#some-of-the-clients-we-work-with .parallax-scroll-container {
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+    background-image: url("http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/uploads/2015/12/s1.jpg");
+}
+
+#some-of-the-clients-we-work-with .parallax-overlay {
+    background-color: rgb(44, 62, 80);
+}
+
+#some-of-the-clients-we-work-with .parallax-overlay {
+    background-color: rgba(44, 62, 80, 0.3);
+}
+
+#our-service {
+    padding-top: 80px;
+    padding-bottom: 40px;
+}
+
+#our-service .ut-offset-anchor {
+    top: -159px;
+}
+
+#our-service .parallax-header {
+    padding-bottom: 30px;
+}
+
+#our-service .section-header {
+    padding-bottom: 30px;
+}
+
+#our-service .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-service .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-service {
+    background-color: #FFFFFF;
+}
+
+#our-service .section-header.pt-style-1 .section-title span {
+    background-color: #FFFFFF;
+}
+
+#our-philosophy-is .parallax-overlay {
+    padding-top: 160px;
+    padding-bottom: 120px;
+}
+
+#our-philosophy-is .ut-offset-anchor {
+    top: -79px;
+}
+
+#our-philosophy-is .parallax-header {
+    padding-bottom: 30px;
+}
+
+#our-philosophy-is .section-header {
+    padding-bottom: 30px;
+}
+
+#our-philosophy-is .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-philosophy-is .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-philosophy-is .parallax-scroll-container {
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+    background-image: url("http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/uploads/2013/10/8625008053_af2ca695a6_b1.jpg");
+}
+
+#our-philosophy-is .parallax-overlay {
+    background-color: rgb(119, 190, 50);
+}
+
+#our-philosophy-is .parallax-overlay {
+    background-color: rgba(119, 190, 50, 0.9);
+}
+
+#our-working-process {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#our-working-process .ut-offset-anchor {
+    top: -159px;
+}
+
+#our-working-process .parallax-header {
+    padding-bottom: 30px;
+}
+
+#our-working-process .section-header {
+    padding-bottom: 30px;
+}
+
+#our-working-process .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-working-process .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#our-working-process {
+    background-color: #F9F9F9;
+}
+
+#our-working-process .section-header.pt-style-1 .section-title span {
+    background-color: #F9F9F9;
+}
+
+#work {
+    padding-top: 80px;
+    padding-bottom: 0px;
+}
+
+#work .ut-offset-anchor {
+    top: -159px;
+}
+
+#work .parallax-header {
+    padding-bottom: 30px;
+}
+
+#work .section-header {
+    padding-bottom: 30px;
+}
+
+#work .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#work .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#work {
+    background-color: #FFFFFF;
+}
+
+#work .section-header.pt-style-1 .section-title span {
+    background-color: #FFFFFF;
+}
+
+#cta {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#cta .ut-offset-anchor {
+    top: -159px;
+}
+
+#cta .parallax-header {
+    padding-bottom: 30px;
+}
+
+#cta .section-header {
+    padding-bottom: 30px;
+}
+
+#cta .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#cta .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#cta {
+    background-color: #F5F5F5;
+}
+
+#cta .section-header.pt-style-1 .section-title span {
+    background-color: #F5F5F5;
+}
+
+#team {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#team .ut-offset-anchor {
+    top: -159px;
+}
+
+#team .parallax-header {
+    padding-bottom: 30px;
+}
+
+#team .section-header {
+    padding-bottom: 30px;
+}
+
+#team .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#team .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#team {
+    background-color: #FFFFFF;
+}
+
+#team .section-header.pt-style-1 .section-title span {
+    background-color: #FFFFFF;
+}
+
+#testimonials .parallax-overlay {
+    padding-top: 80px;
+    padding-bottom: 60px;
+}
+
+#testimonials .ut-offset-anchor {
+    top: -79px;
+}
+
+#testimonials .parallax-header {
+    padding-bottom: 30px;
+}
+
+#testimonials .section-header {
+    padding-bottom: 30px;
+}
+
+#testimonials .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#testimonials .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#testimonials .parallax-scroll-container {
+    background-repeat: !important;
+    background-size: cover !important;
+    background-image: url("http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/uploads/2013/12/p8.jpg");
+}
+
+#testimonials .parallax-overlay {
+    background-color: rgb(44, 62, 80);
+}
+
+#testimonials .parallax-overlay {
+    background-color: rgba(44, 62, 80, 0.3);
+}
+
+#brands {
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+
+#brands .ut-offset-anchor {
+    top: -99px;
+}
+
+#brands .parallax-header {
+    padding-bottom: 30px;
+}
+
+#brands .section-header {
+    padding-bottom: 30px;
+}
+
+#brands .parallax-title {
+    font-family: ralewayextralight, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#brands .section-title {
+    font-family: ralewayextralight, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#brands {
+    background-color: #77BE32;
+}
+
+#brands .section-header.pt-style-1 .section-title span {
+    background-color: #77BE32;
+}
+
+#google-maps {
+    padding-top: 80px;
+    padding-bottom: 0px;
+}
+
+#google-maps .ut-offset-anchor {
+    top: -159px;
+}
+
+#google-maps .parallax-header {
+    padding-bottom: 30px;
+}
+
+#google-maps .section-header {
+    padding-bottom: 30px;
+}
+
+#google-maps .parallax-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+#google-maps .section-title {
+    font-family: ralewaymedium, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.parallax-overlay-pattern.style_one {
+    background-image: url(" http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/themes/brooklyn/images/overlay-pattern.png") !important;
+}
+
+.parallax-overlay-pattern.style_two {
+    background-image: url(" http://themeforest.unitedthemes.com/wpversions/brooklyn/elegant/wp-content/themes/brooklyn/images/overlay-pattern2.png") !important;
+}
+
+#some-of-the-clients-we-work-with h2 {
+    text-transform: uppercase;
+}
+
+.ut-rotate-quote .ut-quote-comment,
+.ut-rotate-quote-alt .ut-quote-comment {
+    font-size: 22px;
+    text-transform: uppercase;
+}
+</style>
