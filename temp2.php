4eecb97dd607f9e8b101e7e5d878f3003241643e
Ampt Design Admin <amptdesign@live.com>
2016-08-14 10:35:05 -0500

    many changes - services.

---
+++ temp2.php
@@ -0,0 +1,32 @@
+[ut_service_column color="#77BE32" hovercolor="#222222" width="third" class="add-bottom" aligncenter]
+<img src="http://localhost:35621/wp-content/themes/brooklyn/images/services-search-engine-optimization.jpg" alt="Search Engine Optimization - SEO" width="360" height="275" class="aligncenter size-large wp-image-3512" />
+<h3 class="serv-title">SEARCH ENGINE OPTIMIZATION</h3><br>
+
+<p class="sweet-justice">We can all agree that every business would love to see their name appear first when any customer types their name into Google or any other search engine.</p><p class="sweet-justice"> Unfortunately, ten’s of millions of other businesses want the same results and many are willing to pay to get to the top. We do our best to work with whatever budget you have to get your website to appear as close to the top as we can possibly get it!</p>
+
+[ut_button color="green" target="_self" link="#" size="medium" ]Learn More[/ut_button]
+
+[/ut_service_column]
+&nbsp;
+[ut_service_column color="#77BE32" hovercolor="#222222" headline="CREATE CUSTOMER INTERACTION" width="third" class="add-bottom"]
+
+<p class="sweet-justice">Successful businesses increase their value by creating customer interaction through integrated social networks like Facebook, Twitter, LinkedIn, Pinterest or Instagram.</p>
+&nbsp;
+[ut_button color="green" target="_self" link="#" size="medium" ]OUR QUALITY STANDARDS[/ut_button]
+
+&nbsp;
+
+<img class="alignnone size-full wp-image-3451" src="http://localhost:35621/wp-content/themes/brooklyn/images/services-social-media-networks.jpg" alt="Services - Social Media Networks" width="360" height="275" />
+
+[/ut_service_column]
+
+[ut_service_column color="#77BE32" hovercolor="#222222" width="third" class="add-bottom"]
+<img src="http://localhost:35621/wp-content/themes/brooklyn/images/services-security-backup.jpg" alt="Security, Backup, &amp; Maintenance" width="320" height="280" class="aligncenter size-full wp-image-3519" />
+
+<h3 class="serv-title">SECURITY & BACKUP SERVICES</h3><br>
+
+<p class="sweet-justice">Although all our websites come with industry standard security and basic backup services.</p><p class="sweet-justice"> We also offer, advanced security and backup services for companies that have special security needs and/or highly sensitive data.</p>
+
+[ut_button color="green" target="_self" link="#" size="medium" ]Learn More[/ut_button]
+
+[/ut_service_column]
